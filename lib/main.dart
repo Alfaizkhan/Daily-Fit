70b2ef3ea537f99c4705603c0745bec8a0547a21
Alfaizkhan <alfaizkhan2810@gmail.com>
2022-07-23 22:41:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'AgentDuty',
+      title: 'DailyFit',
       theme: ThemeData(
           visualDensity: VisualDensity.adaptivePlatformDensity,
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
